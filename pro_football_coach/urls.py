11526d82d88c977fbe6a22aee98fc5501ca96048
Patryk <pat.parkitny@gmail.com>
2023-01-07 22:20:40 +0100

    First steps with djangorestframework

--- pro_football_coach/urls.py
+++ pro_football_coach/urls.py
@@ -14,13 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from . import views
-from django.conf import settings
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('amateur_teams.urls')),
-    path('', views.getPlayer),
-    path('post/', views.getPlayer),
 ]
