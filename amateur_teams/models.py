7bb60569e0febf6788ce902b352832ae421e337f
Patryk <pat.parkitny@gmail.com>
2023-01-09 19:00:02 +0100

    Add descriptions to player stats

--- amateur_teams/models.py
+++ amateur_teams/models.py
@@ -60,89 +60,96 @@ class Stats(models.Model):
 
 
 class Pace(models.Model):
+    # The speed with which a player walks or runs.
     sprint_speed = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # Defines the speed rate of a player’s sprinting.
     acceleration = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # The rate at which a player’s running speed increases.
 
 
 class Shoting(models.Model):
+    # A player’s general shooting strength and accuracy.
     finishing = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # The ability of a player to score.
     positioning = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # Defines a player’s ability to spot open space and move into good positions that offer an attacking advantage
     shot_power = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # The strength of a player’s shoots.
     long_shots = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # A player’s accuracy for the shots taking from long distances.
     penalties = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # A player’s accuracy for taking penalty shots.
     volleys = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # The ability of a player to perform volleys.
 
 
 class Passing(models.Model):
+    # Determines how accurate a player passes the ball to a teammate
     vision = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # A player’s mental awareness about his teammates’ positioning, for passing the ball to them.
     crossing = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # The quality and accuracy of a player’s crosses.
     fk_accuracy = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # The precision with which a player takes free kicks.
     long_passing = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # A player’s accuracy for the long passes.
     short_passing = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # A player’s accuracy for the short passes.
     curve = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # A player’s ability to curve the ball when passing and shooting.
 
 
 class Dribling(models.Model):
+    # A player’s ability to carry the ball and past an opponent.
     agility = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # Determines a player’s ability to manage and control the ball quickly and gracefully.
     balance = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # The even distribution of enabling a player to remain upright and keep going.
     reactions = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # A player’s reaction time in response to events taking place around them.
     composure = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # A player’s the state or feeling of being calm and in control of their frustration.
     ball_control = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # A player’s ability to keep possession of the ball.
     dribbling = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # A player’s ability to carry the ball and past an opponent.
 
 
 class Defending(models.Model):
+    # A player’s ability to defend.
     interceptions = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # The ability of a player to intercept the ball.
     heading_accuracy = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # A player’s accuracy when using the head to pass, shoot or clear the ball.
     defensive_awareness = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # A player’s ability to defend.
     standing_tackle = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # The ability of performing standing tackle.
     sliding_tackle = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # The ability to pull off a sliding tackle.
 
 
 class Physicality(models.Model):
+    # Represent the physical and bodily state of a player.
     jumping = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # The ability of a player to jump off the ground for headers.
     stamina = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # player’s ability to sustain prolonged physical or mental effort
     strength = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # The quality or state of being physically strong.
     aggression = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # A player’s degree of aggressiveness.
 
 
 class Goalkeeping(models.Model):
+    # A player’s ability to be a goalkeeper
     gk_diving = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # Ability to 'dive' for a ball
     gk_handling = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # Ability to catch the ball
     gk_kicking = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # Ability to make long and accurate kicks of the ball
     gk_positioning = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # Ability to position as goalkeeper
     gk_reflexes = models.PositiveIntegerField(
-        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])
+        default=1, validators=[MinValueValidator(1), MaxValueValidator(99)])  # A goalkeeper reaction time in response to events taking place around him.
