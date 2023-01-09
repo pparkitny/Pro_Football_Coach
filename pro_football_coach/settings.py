3702d3e15480af1a79ba4706e7375fe2cf0de38b
Patryk <pat.parkitny@gmail.com>
2023-01-09 19:51:30 +0100

    Add psql database, make migrations to db

--- pro_football_coach/settings.py
+++ pro_football_coach/settings.py
@@ -77,13 +77,22 @@ WSGI_APPLICATION = 'pro_football_coach.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'HOST': '127.0.0.1',
+        'NAME': 'coach_db',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': 'postgres2',
+        'PASSWORD': 'localhost',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
