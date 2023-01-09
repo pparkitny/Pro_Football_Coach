0b40d12e426af1ec8c50d9f51c2098bc798e2f8b
Patryk <pat.parkitny@gmail.com>
2023-01-09 12:35:27 +0100

    Add REST app to settings

--- pro_football_coach/settings.py
+++ pro_football_coach/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
+# this is to replace the secret key you cut away before
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'amateur_teams',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
